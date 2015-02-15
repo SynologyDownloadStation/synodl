6833354d38b7ecba3be6aa942f0128340a239e34
Stefan Ott <stefan@ott.net>
2015-02-15 23:01:34 +0100

    Added some getopt magic

--- src/synodl.c
+++ src/synodl.c
@@ -18,17 +18,67 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "config.h"
 #include "cfg.h"
 #include "http.h"
 
+void help()
+{
+	printf("Syntax: synodl [options] [URL]\n\n");
+	printf("If URL is empty a list of current download tasks is shown,\n");
+	printf("otherwise the URL is added as a download task.\n\n");
+	printf("  -h, --help           Show this help\n");
+	printf("      --dump           Show current downloads (default)\n");
+	printf("\n");
+	printf("This is %s.\n", PACKAGE_STRING);
+	printf("Report bugs at https://github.com/cockroach/synodl/\n");
+}
+
 int main(int argc, char **argv)
 {
-	struct cfg config;
+	int c, option_idx;
 	const char *url;
+	struct cfg config;
+	struct session s;
+
+	struct option long_options[] =
+	{
+		{"help", no_argument, 0, 'h' },
+		{"dump", no_argument, 0, 0 },
+		{0,      0,           0, 0 }
+	};
+
+	while (1)
+	{
+		c = getopt_long(argc, argv, "h", long_options, &option_idx);
+
+		if (c == -1)
+		{
+			break;
+		}
+
+		switch (c)
+		{
+		case 0:
+			switch (option_idx)
+			{
+			case 1:
+				// dump -> default operation, ignoring
+				break;
+			}
+		case 'h':
+			help();
+			return EXIT_SUCCESS;
+		default:
+			help();
+			return EXIT_FAILURE;
+		}
+	}
 
 	memset(&config, 0, sizeof(struct cfg));
 
@@ -38,7 +88,6 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	struct session s;
 	memset(&s, 0, sizeof(struct session));
 
 	if (login(config.url, &s, config.user, config.pw) != 0)
@@ -46,9 +95,9 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	if (argc > 1)
+	if (optind < argc)
 	{
-		url = argv[1];
+		url = argv[optind];
 		download(config.url, &s, url);
 	}
 	else
